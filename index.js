0b2d755afff3d42fee8e1025ad01402a2473c3f4
Ernazar dev <maksetovernazar828@gmail.com>
2024-03-18 23:00:05 +0500

    comleted #1,2,3 lessons

---
+++ index.js
@@ -0,0 +1,27 @@
+
+import getArgs from './helpers/args.js'
+import { printError,printSuccess, printHelp } from './services/log.service.js'
+
+
+const startCLI = () => 
+{
+  const args = getArgs(process.argv)
+  console.log(args);
+  if(args.h)
+  {
+    printHelp()
+    // help
+  }
+  if (args.s) 
+  {
+		// save city
+	}
+  if (args.t) 
+  {
+		// save token
+	}
+  // result
+  }
+
+
+startCLI()
