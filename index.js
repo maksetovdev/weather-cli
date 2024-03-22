556409805d32f15384bb9481041208f977895b03
Ernazar dev <maksetovernazar828@gmail.com>
2024-03-22 06:19:16 +0500

    fix bug & use axios library

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 
 import getArgs from './helpers/args.js'
 import { printError,printSuccess, printHelp } from './services/log.service.js'
+import { saveToken, TOKEN_DICTIONARY } from './services/storage.service.js'
+import { getWeather } from './services/api.service.js'
 
+const Token = async token => {
+
+  if(!token.length) {
+    printError("Token doesn`t exist")
+    return 
+  }
+  try {
+    await saveToken(TOKEN_DICTIONARY.token, token)
+    printSuccess('Token was saved!')
+  } catch (error) {
+    printError(error.message)
+  }
+}
 
 const startCLI = () => 
 {
   const args = getArgs(process.argv)
-  console.log(args);
+  // console.log(args);
   if(args.h)
   {
     printHelp()
@@ -18,9 +33,11 @@ const startCLI = () =>
 	}
   if (args.t) 
   {
+    return Token(args.t);
 		// save token
 	}
   // result
+  getWeather('Nukus')
   }
 
 
---
+++ services/api.service.js
@@ -0,0 +1,49 @@
+
+import https from 'https'
+import {getToken, TOKEN_DICTIONARY} from './storage.service.js'
+import axios from 'axios'
+
+const getWeather = async city => {
+	//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
+	const token = await getToken(TOKEN_DICTIONARY.token)
+
+	if (!token) {
+		throw console.error('API doesn`t exist or token was invalid')
+	}
+
+	//! With Axios
+
+	const {data} = await axios.get('https://api.openweathermap.org/data/2.5/weather', {
+		params: {
+			q: city,
+			appid: token,
+			lang: 'ru',
+			units: 'metric'
+		}
+	})
+	return data
+	
+	//! With https 
+
+	//https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+	// const url = new URL(
+	// 	'https://api.openweathermap.org/data/2.5/weather'
+	// )
+	// url.searchParams.append('q', city)
+	// url.searchParams.append('appid', token)
+	// url.searchParams.append('lang', 'en')
+	// url.searchParams.append('units', 'metric')
+
+	// https.get(url, response => {
+	// 	let res = ''
+	// 	response.on('data', chunk => {
+	// 		res += chunk
+	// 	})
+	// 	response.on('end', () => {
+	// 		console.log(res)
+	// 	})
+	// })
+
+}
+
+export {getWeather}
